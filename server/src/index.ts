ce202056af3d3a4c09f51321720ac2a9631432b9
Wilder Rincon <1588@holbertonschool.com>
2021-02-11 14:56:48 -0500

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from 'body-parser'
+import { listings } from './listings'
+
+const app = express();
+const port = 9000;
+
+app.use(bodyParser.json())
+
+const one = 1;
+const two = 2;
+
+app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
+app.get("/listings", (_req, res) => {
+    return res.send(listings)
+} )
+
+app.post("/delete-listing", (req, res) => {
+    const id = req.body.id
+
+    for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1))
+        }
+    }
+    return res.send("listings")
+})
+
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
